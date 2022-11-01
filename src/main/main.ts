c4a7a42cbe3b7d1e8c3023b86523677c16edccda
nhuydev1704 <khanhandli@gmail.com>
2022-11-01 20:48:38 +0700

    add mui

--- src/main/main.ts
+++ src/main/main.ts
@@ -71,8 +71,8 @@ const createWindow = async () => {
 
   mainWindow = new BrowserWindow({
     show: false,
-    width: 1024,
-    height: 728,
+    width: 1366,
+    height: 768,
     icon: getAssetPath('icon.png'),
     webPreferences: {
       preload: app.isPackaged
